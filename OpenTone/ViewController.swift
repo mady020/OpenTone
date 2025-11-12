b2a44a9aadd74d30b857d8542d31a0315a32f554
mady020 <=>
2025-11-12 09:29:40 +0530

    Ready Set Go

---
+++ OpenTone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenTone
+//
+//  Created by Student on 12/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
