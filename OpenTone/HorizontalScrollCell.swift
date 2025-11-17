7414e61ddf5f0cd9ccd0e90a0b448beb3c1e01e7
Harsh1841 <156991776+Harsh1841@users.noreply.github.com>
2025-11-17 09:24:01 +0530

    work on dashboard  + 1 to 1 call (#1)

---
+++ OpenTone/HorizontalScrollCell.swift
@@ -0,0 +1,23 @@
+//
+//  HorizontalScrollCell.swift
+//  OpenTone
+//
+//  Created by Harshdeep Singh on 14/11/25.
+//
+
+import UIKit
+
+class HorizontalScrollCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ OpenTone/SelectableCell.swift
@@ -0,0 +1,12 @@
+//
+//  SelectableCell.swift
+//  OpenTone
+//
+//  Created by Harshdeep Singh on 16/11/25.
+//
+
+import UIKit
+
+class SelectableCell: UICollectionViewCell {
+    
+}
