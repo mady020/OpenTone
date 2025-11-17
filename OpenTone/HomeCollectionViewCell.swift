2688a373d72cf198a3442e98f9385bdcce14a2f0
Harsh1841 <156991776+Harsh1841@users.noreply.github.com>
2025-11-17 09:24:01 +0530

    work on dashboard  + 1 to 1 call (#1)

---
+++ OpenTone/HomeCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HomeCollectionViewCell.swift
+//  OpenTone
+//
+//  Created by Harshdeep Singh on 13/11/25.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+    
+}
