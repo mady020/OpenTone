7fa40cdbe7e4705b0793cd7d7fe7fd28061c69a5
Harsh1841 <harsh1841.be23@chitkara.edu.in>
2025-11-17 09:22:39 +0530

    work on dashboard  + 1 to 1 call

---
+++ OpenTone/SectionHeaderView.swift
@@ -0,0 +1,12 @@
+//
+//  SectionHeaderView.swift
+//  OpenTone
+//
+//  Created by Harshdeep Singh on 16/11/25.
+//
+
+import UIKit
+
+class SectionHeaderView: UICollectionReusableView {
+        
+}
