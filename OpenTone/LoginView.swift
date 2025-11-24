d5d8c794f4d2a3001ec58ea513364eb0cec681d9
Keerti <keerti0460.be23@chitkara.edu.in>
2025-11-24 19:01:04 +0530

    next step
    started work for data passing

---
+++ OpenTone/LoginView.swift
@@ -0,0 +1,27 @@
+//
+//  LoginView.swift
+//  OpenTone
+//
+//  Created by Student on 24/11/25.
+//
+
+//import Foundation
+//
+//class LoginViewController: UIViewController {
+//    
+//    @IBOutlet weak var usernameField: UITextField!
+//    @IBOutlet weak var passwordField: UITextField!
+//    
+//    var username: String?
+//    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "goToHome" {
+//            let destinationVC = segue.destination as! HomeViewController
+//            destinationVC.username = usernameField.text
+//        }
+//    }
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        print("Logged in user: \(username ?? "")")
+//    }
+//}
