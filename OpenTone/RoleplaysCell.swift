381cc4da68e8adc77cdbe3f089286a8dcc1dfaaf
Harsh1841 <harsh1841.be23@chitkara.edu.in>
2025-11-26 20:10:20 +0530

    roleplays first screen

--- OpenTone/RoleplaysCell.swift
+++ OpenTone/RoleplaysCell.swift
@@ -1,12 +1,27 @@
-//
-//  RoleplaysCell.swift
-//  OpenTone
-//
-//  Created by Harshdeep Singh on 26/11/25.
-//
-
 import UIKit
 
 class RoleplaysCell: UICollectionViewCell {
+
+    @IBOutlet weak var roleplayImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        contentView.layer.cornerRadius = 30
+        contentView.clipsToBounds = true
+
+ 
+        roleplayImageView.contentMode = .scaleAspectFill
+        roleplayImageView.clipsToBounds = true
+
     
+        titleLabel.alpha = 0.4
+
+    }
+
+    func configure(title: String, imageName: String) {
+        titleLabel.text = title
+        roleplayImageView.image = UIImage(named: imageName)
+    }
 }
