3ea17d7116aa8b38fb7fcf6ca5d84a6c809199fb
Keerti <keerti0460.be23@chitkara.edu.in>
2025-12-03 12:25:11 +0530

    StartJamUI
    ui is done , animations needs to be added

---
+++ OpenTone/StartJamViewController.swift
@@ -0,0 +1,36 @@
+//
+//  StartJamViewController.swift
+//  OpenTone
+//
+//  Created by Student on 03/12/25.
+//
+
+import UIKit
+
+class StartJamViewController: UIViewController {
+
+    @IBOutlet weak var bulbButton: UIButton!
+   
+    @IBOutlet weak var micButton: UIButton!
+    
+   
+    @IBOutlet weak var cancelButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
