a97176faa3b152b4b680feaaf1551efa4013bccf
Tanreet Kaur <tanreet0902.be23@chitkara.edu.in>
2025-12-06 12:06:08 +0530

    ui changed

---
+++ OpenTone/streak-progess/WeekDayCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  WeekDayCollectionViewCell.swift
+//  OpenTone
+//
+//  Created by Jashanpreet Singh on 06/12/25.
+//
+
+import UIKit
+
+class WeekDayCollectionViewCell: UICollectionViewCell {
+    
+}
--- OpenTone/streak-progess/homeViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  homeViewController.swift
-//  OpenTone
-//
-//  Created by Student on 05/12/25.
-//
-
-import Foundation
